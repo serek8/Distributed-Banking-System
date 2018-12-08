cf75007da57616706ad7b0f59ef3fe6f835bee9b
Alexander Blaauwgeers <Alexander@Blaauwgeers.eu>
2018-12-08 16:52:11 +0100

    Testscript in functions
    Signed-off-by: Alexander Blaauwgeers <Alexander@Blaauwgeers.eu>

--- testpy/testing.py
+++ testpy/testing.py
@@ -1,35 +1,124 @@
+#####################################
+###  Testing v1
+###  a=bab2501 v= 1.0 d= 2018.12.08
+####################################
+
+###
+# Dependencies 
+# => pip install requests
+#########################
 import requests
 
-# Senerio A: Too account with same number
-r = requests.post('http://127.0.0.1:8080/accounts/create',json={"accountNumber" : "1"})
-r = requests.post('http://127.0.0.1:8080/accounts/create',json={"accountNumber" : "1"})
+###
+# Global Settings
+#########################
 
-# Senerio B:  
-r = requests.post('http://127.0.0.1:8080/accounts/1/deposit',json={"amount" : "200"})
-r = requests.post('http://127.0.0.1:8080/accounts/1/withdraw',json={"amount" : "50"}) #should happen
-r = requests.post('http://127.0.0.1:8080/accounts/1/withdraw',json={"amount" : "50"}) #should happen
-r = requests.post('http://127.0.0.1:8080/accounts/1/withdraw',json={"amount" : "50"}) #should happen
-r = requests.post('http://127.0.0.1:8080/accounts/1/withdraw',json={"amount" : "50"}) #should happen
-r = requests.post('http://127.0.0.1:8080/accounts/1/withdraw',json={"amount" : "150"}) #should NOT happen
+host = "127.0.0.1"
+protocol = "http"
+port = "8080"
+baseurl = protocol + "://" + host + ":" + port
 
-# Senerio C:  
-r = requests.post('http://127.0.0.1:8080/accounts/1/deposit',json={"amount" : "400"}) #should happen
-r = requests.post('http://127.0.0.1:8080/accounts/1/withdraw',json={"amount" : "200"}) #should happen
+###
+# Standard Functions (theAPI) 
+#########################
+def createAccount(accountNumber):
+	url = baseurl+"/accounts/create"
+	jdata={"accountNumber": str(accountNumber)}
+	r = requests.post(url,json=jdata)
+	print("Status: " + str(r.status_code) + " Message: " + r.text + " -- createAccount " + str(accountNumber))
 
-# Senerio Ca: More then one  
-while True:
-r = requests.post('http://127.0.0.1:8080/accounts/1/deposit',json={"amount" : "400"}) #should happen
-r = requests.post('http://127.0.0.1:8080/accounts/1/withdraw',json={"amount" : "200"}) #should happen
-r = requests.post('http://127.0.0.1:8080/accounts/1/withdraw',json={"amount" : "200"}) #should happen
-#break;
+def balanceAccount(accountNumber):
+	url = baseurl+"/accounts/"+str(accountNumber)+"/balance"
+	#print(url)
+	#jdata={"amount" : str(amount)}
+	#r = requests.post(url,json=jdata)
+	r = requests.get(url)
+	print("Status: " + str(r.status_code) + " Message: " + r.text)
+	return float(r.text);
 
-# Senerio D:
-r = requests.post('http://127.0.0.1:8080/accounts/1/deposit',json={"amount" : "100"}) #should happen
-r = requests.post('http://127.0.0.1:8080/accounts/1/transfer',json={"amount" : "100","accountNumberDestination" : "2"}) #should happen
-r = requests.post('http://127.0.0.1:8080/accounts/1/withdraw',json={"amount" : "100"}) #should NOT happen
-r = requests.post('http://127.0.0.1:8080/accounts/2/withdraw',json={"amount" : "100"}) #should happen
+def depositAccount(accountNumber, amount):
+	url = baseurl+"/accounts/"+str(accountNumber)+"/deposit"
+	jdata={"accountNumber," : str(accountNumber), "amount" : amount}
+	#print(url)
+	r = requests.post(url,json=jdata)
+	print("Status: " + str(r.status_code) + " Message: " + r.text + " -- depositAccount " + str(amount) + " to " + str(accountNumber))
 
+def withdrawAccount(accountNumber, amount):
+	url = baseurl+"/accounts/"+str(accountNumber)+"/withdraw"
+	jdata={"amount" : amount}
+	#print(url)
+	r = requests.post(url,json=jdata)
+	print("Status: " + str(r.status_code) + " Message: " + r.text + " -- withdrawAccount " + str(amount) + " from " + str(accountNumber))
 
-http://127.0.0.1:8080/accounts/1/transfer 
-print(r.text)
-print(r.headers)
+def transferAccount(accountNumber, amount, accountNumberDestination):
+	url = baseurl+"/accounts/"+str(accountNumber)+"/transfer"
+	jdata={"amount" : amount, "accountNumberDestination" : str(accountNumberDestination)}
+	#print(url)	
+	r = requests.post(url,json=jdata)
+	print("Status: " + str(r.status_code) + " Message: " + r.text + " -- transferAccount " + str(amount) + " from " + str(accountNumber) + " to " + str(accountNumberDestination))
+	
+###
+# Scenario
+#########################
+
+def scenarioA(accountNumber = 1):
+	print("= senerioA =")
+	balanceAccount(accountNumber)
+	createAccount(accountNumber)
+	balanceAccount(accountNumber)
+	createAccount(accountNumber)
+	balanceAccount(accountNumber)
+
+def scenarioB(accountNumber = 1):
+	print("= senerioB =")
+	balanceAccount(accountNumber)
+	depositAccount(accountNumber, 200)  #should happen
+	withdrawAccount(accountNumber, 50)  #should happen
+	withdrawAccount(accountNumber, 50)  #should happen
+	withdrawAccount(accountNumber, 50)  #should happen
+	withdrawAccount(accountNumber, 50)  #should happen
+	withdrawAccount(accountNumber, 150) #should NOT happen
+	balanceAccount(accountNumber)
+
+def scenarioC(accountNumber = 1):
+	print("= senerioC =")
+	while True:
+		depositAccount(accountNumber, 400)  #should happen
+		withdrawAccount(accountNumber, 200)  #should happen
+		withdrawAccount(accountNumber, 200)  #should happen
+		break;
+	balanceAccount(accountNumber)
+	print("end senerioC")
+
+def scenarioD(accountNumber = 1,accountNumberDestination = 2, amount = 100):
+	print("= senerioD =")
+	balanceAccount(accountNumber)
+	balanceAccount(accountNumberDestination)
+	while True:
+		depositAccount(accountNumber, amount)  						#should happen
+		transferAccount(accountNumber, amount, accountNumberDestination)	#should happen
+		withdrawAccount(accountNumber, amount)						#should NOT happen
+		withdrawAccount(accountNumberDestination, amount)			#should happen
+		break;
+	balanceAccount(accountNumber)
+	balanceAccount(accountNumberDestination)
+	print("end senerioD")
+
+## Setup
+createAccount(1)
+createAccount(2)
+balanceAccount(1)
+balanceAccount(2)
+
+scenarioA();
+scenarioB();
+scenarioC();
+scenarioD();
+
+print("reset");
+balanceAccount(1)
+balanceAccount(2)
+depositAccount(1, 100)
+withdrawAccount(2, 100)
+balanceAccount(1)
+balanceAccount(2)
